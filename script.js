390c0eb4c71d01263ad302e3796f4863cb0717ed
cahoward89 <124751864+cahoward89@users.noreply.github.com>
2026-01-26 10:55:13 -0500

    Create computer choice part of code

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello, World!")
+// Your game will be played against the computer. You will write a function that randomly returns “rock”, “paper” or “scissors”.
+// Write the code so that getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”.
+// use math.random() and use if else statements to make .0-.33 rock, .34-.66 paper, and .67-.99 scissors
+
+function getComputerChoice() {
+    const R = Math.random();
+    if (R<1/3) {return "rock";}
+    else if (R<2/3) {return "paper";}
+    else {return "scissors";}
+}
+
+console.log(getComputerChoice())
