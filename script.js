50f5d2d23a1f6feeebed53edafac7dade9ed7194
cahoward89 <124751864+cahoward89@users.noreply.github.com>
2026-01-28 10:13:39 -0500

    Code the human choice function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Your game will be played against the computer. You will write a function that randomly returns “rock”, “paper” or “scissors”.
 // Write the code so that getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”.
-// use math.random() and use if else statements to make .0-.33 rock, .34-.66 paper, and .67-.99 scissors
+// Use math.random() and use if else statements to make .0-.33 rock, .34-.66 paper, and .67-.99 scissors
 
 function getComputerChoice() {
     const R = Math.random();
@@ -10,3 +10,12 @@ function getComputerChoice() {
 }
 
 console.log(getComputerChoice())
+
+//The game will be played by a human player. You will write a function that takes the user choice and returns it.
+
+function getHumanChoice () {
+    const user=prompt("Please enter Rock, Paper, or Scissors")
+    return user
+}
+
+console.log(getHumanChoice())
