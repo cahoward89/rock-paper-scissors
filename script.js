9dccba8e071c64d646ea4dcd6a8f80e2b218e0ec
cahoward89 <124751864+cahoward89@users.noreply.github.com>
2026-02-12 17:21:54 -0500

    Add UI elements and change code to use them

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// Your game will be played against the computer. You will write a function that randomly returns “rock”, “paper” or “scissors”.
-// Write the code so that getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”.
-// Use math.random() and use if else statements to make .0-.33 rock, .34-.66 paper, and .67-.99 scissors
 
 function getComputerChoice() {
     const R = Math.random();
@@ -10,25 +7,21 @@ function getComputerChoice() {
 }
 
 
-//The game will be played by a human player. You will write a function that takes the user choice and returns it.
 
-function getHumanChoice () {
-    const user=prompt("Please enter Rock, Paper, or Scissors")
-    return user.toLowerCase()
-}
+const buttons = document.querySelectorAll(".button-set button");
+buttons.forEach(button => {
+    button.addEventListener("click", ()=> {
+        const humanPick = button.textContent.toLowerCase();
+        let computerPick = getComputerChoice();
+        playRound(humanPick,computerPick);
+        updateComputerChoice(computerPick);
+        updateScoreboard();
+        if (humanScore==5) {console.log("You are the Winner! Great Job!")};
+        if (computerScore==5) {console.log("The computer Wins! Better luck next time!")};
+    })
+})
 
 
-let humanScore=0
-let computerScore=0
-
-//Your game will be played round by round. You will write a function that takes the human and computer player choices as arguments, plays a single round, increments the round winner’s score and logs a winner announcement.
-
-//I don't want these to run outside of my gameloop
-
-//let humanPick=getHumanChoice();
-//let computerPick=getComputerChoice();
-
-//
 
 function playRound(humanPick, computerPick) {
 
@@ -43,30 +36,32 @@ function playRound(humanPick, computerPick) {
     }
     else {computerScore+=1; 
         return "You Lose this one!"}
+    
+}
+
+let humanScore=0
+let computerScore=0
+
+let para = document.createElement("p");
+para.textContent = `Your Score: ${humanScore} ||| Computer Score: ${computerScore}`
+let scoreboard = document.querySelector(".scoreboard");
+scoreboard.appendChild(para);
+
+function updateScoreboard() {
+  para.textContent = `Your Score: ${humanScore} ||| Computer Score: ${computerScore}`;
 }
 
 
-// Your game will play 5 rounds, keep track of the scores and declare a winner at the end.
+let computerChoice = document.createElement("p");
+        scoreboard.appendChild(computerChoice);
 
-function playGame (){
-    for (let i = 0; i < 5; i++) {
 
-        let humanPick=getHumanChoice();
-        let computerPick=getComputerChoice();
-        let result=playRound(humanPick,computerPick)
-
-        
-        console.log("You chose " + humanPick)
-        console.log("The computer chose " + computerPick)
-        console.log(result)
-        console.log("Your Score:" + humanScore)
-        console.log("Computer Score:"+computerScore)
-        console.log("-------------------------")
-    }
-
-    if (humanScore>computerScore) {console.log("You are the Winner! Great Job!");}
-    else if (computerScore>humanScore) {console.log("The computer Wins! Better luck next time!")}
-    else {console.log("The game ends in a tie.")}
+function updateComputerChoice(computerPick){
+    computerChoice.textContent = `The computer chose ${computerPick}`;
 }
 
-playGame()
+
+
+
+
+
